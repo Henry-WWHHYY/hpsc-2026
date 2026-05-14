91264b910c5926ca85228ffae5ba274327794cca
Henry-WWHHYY <135588215+Henry-WWHHYY@users.noreply.github.com>
2026-05-14 23:27:31 +0900

    Update 13_bucket_sort.cpp

--- 06_cuda/13_bucket_sort.cpp
+++ 06_cuda/13_bucket_sort.cpp
@@ -2,31 +2,47 @@
 #include <cstdlib>
 #include <vector>
 
+__global__ void bucket_count(int *bucket, int *key, int n) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i >= n) return;
+  atomicAdd(&bucket[key[i]], 1);
+}
+
 int main() {
   int n = 50;
   int range = 5;
-  std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
+
+  int *key;
+  int *bucket;
+  cudaMallocManaged(&key, n * sizeof(int));
+  cudaMallocManaged(&bucket, range * sizeof(int));
+
+  for (int i = 0; i < n; i++) {
     key[i] = rand() % range;
-    printf("%d ",key[i]);
+    printf("%d ", key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
+  for (int i = 0; i < range; i++) {
     bucket[i] = 0;
   }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
+
+  const int blockSize = 128;
+  const int gridSize  = (n + blockSize - 1) / blockSize;
+  bucket_count<<<gridSize, blockSize>>>(bucket, key, n);
+  cudaDeviceSynchronize();
+
+  for (int i = 0, j = 0; i < range; i++) {
+    for (; bucket[i] > 0; bucket[i]--) {
       key[j++] = i;
     }
   }
 
-  for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", key[i]);
   }
   printf("\n");
+
+  cudaFree(key);
+  cudaFree(bucket);
 }
