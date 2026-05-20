72e2de60dfd9cb9f765069040a034cbe1798c197
Henry-WWHHYY <135588215+Henry-WWHHYY@users.noreply.github.com>
2026-05-20 13:29:46 +0900

    a fix

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -5,7 +5,7 @@
 
 int main() {
   const int N = 16;
-  alignas(64) float x[N], y[N], m[N], fx[N], fy[N];
+  float x[N], y[N], m[N], fx[N], fy[N];
   for(int i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
@@ -13,27 +13,28 @@ int main() {
     fx[i] = fy[i] = 0;
   }
 
-  __m512 xv = _mm512_load_ps(x);
-  __m512 yv = _mm512_load_ps(y);
-  __m512 mv = _mm512_load_ps(m);
+  __m512 xvec = _mm512_loadu_ps(x);
+  __m512 yvec = _mm512_loadu_ps(y);
+  __m512 mvec = _mm512_loadu_ps(m);
+
+  __m512i jidx = _mm512_set_epi32(15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0);
 
   for(int i=0; i<N; i++) {
-    __m512 xi  = _mm512_set1_ps(x[i]);
-    __m512 yi  = _mm512_set1_ps(y[i]);
-    __m512 rx  = _mm512_sub_ps(xi, xv);
-    __m512 ry  = _mm512_sub_ps(yi, yv);
-    __m512 r2  = _mm512_add_ps(_mm512_mul_ps(rx, rx),
-                               _mm512_mul_ps(ry, ry));
-    __mmask16 mask = _mm512_cmpneq_epi32_mask(
-        _mm512_set1_epi32(i),
-        _mm512_setr_epi32(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15));
-    __m512 r       = _mm512_sqrt_ps(r2);
-    __m512 r3      = _mm512_mul_ps(r2, r);
-    __m512 invr3   = _mm512_maskz_div_ps(mask, mv, r3);
-    __m512 fxv     = _mm512_mul_ps(rx, invr3);
-    __m512 fyv     = _mm512_mul_ps(ry, invr3);
-    fx[i] = -_mm512_reduce_add_ps(fxv);
-    fy[i] = -_mm512_reduce_add_ps(fyv);
+    __m512 xi = _mm512_set1_ps(x[i]);
+    __m512 yi = _mm512_set1_ps(y[i]);
+    __m512 rx = _mm512_sub_ps(xi, xvec);
+    __m512 ry = _mm512_sub_ps(yi, yvec);
+    __m512 r2 = _mm512_fmadd_ps(rx, rx, _mm512_mul_ps(ry, ry));
+    __m512 invr  = _mm512_rsqrt14_ps(r2);
+    __m512 invr3 = _mm512_mul_ps(invr, _mm512_mul_ps(invr, invr));
+    __m512i ivec = _mm512_set1_epi32(i);
+    __mmask16 mask = _mm512_cmpneq_epi32_mask(jidx, ivec);
+    __m512 coeff = _mm512_mul_ps(mvec, invr3);
+    __m512 dfx   = _mm512_mul_ps(rx, coeff);
+    __m512 dfy   = _mm512_mul_ps(ry, coeff);
+    fx[i] -= _mm512_mask_reduce_add_ps(mask, dfx);
+    fy[i] -= _mm512_mask_reduce_add_ps(mask, dfy);
+
     printf("%d %g %g\n", i, fx[i], fy[i]);
   }
 }
